7e4e5f0ee26544ca631b1f3fbf46ad6ba536f301
RomanBykov <rmnbykov@gmail.com>
2018-07-17 17:23:41 +0300

    Поработал над js
    Исправил вёрстку кнопок. Нужно поправить реакцию на нажатие кнопки "Купить" на главной странице

--- js/script.js
+++ js/script.js
@@ -1,34 +1,37 @@
 'use strict';
 
-try {
+var pageBody = document.querySelector('body');
+var modalContacts = document.querySelector('.modal-contacts');
+var openContactsButton = document.querySelector('.contacts-btn');
+var closeContactsButton = document.querySelector('.close-contacts-btn');
+var slideButtonActive = 'services-btn-active';
+var slideActive = 'slide-active';
+var elementActive = 'element-active';
+var keyCodes = {
+  ESC: 27,
+  ENTER: 13,
+  SPACE: 27
+};
+
+var changeSlide = function (slideButton, slideName) {
+  slideButton.addEventListener('click', function() {
+    resetSlider();
+    slideButton.classList.add(slideButtonActive);
+    slideName.classList.add(slideActive);
+  })
+};
+
+if (document.querySelector('.modal-map')) {
+  var modalMap = document.querySelector('.modal-map');
   var openMapButton = document.querySelector('.map-btn');
-  var closeMapButton = document.querySelector('.close-map-btn');
-  var closeContactsButton = document.querySelector('.close-contacts-btn');
-  var servicesSlides = document.querySelector('.services-slides');
-  var slideButtonOne = document.querySelector('#slide-1');
-  var slideButtonTwo = document.querySelector('#slide-2');
-  var slideButtonThree = document.querySelector('#slide-3');
-  var servicesSlideOne = document.querySelector('.services-slide-1');
-  var servicesSlideTwo = document.querySelector('.services-slide-2');
-  var servicesSlideThree = document.querySelector('.services-slide-3');
-
-  var slideButtons = [slideButtonOne, slideButtonTwo, slideButtonThree];
-  var slides = [servicesSlideOne, servicesSlideTwo, servicesSlideThree];
-
-  var slideButtonActive = 'services-btn-active';
-  var slideActive = 'slide-active';
-
-  openMapButton.addEventListener('click', function() {
-    modalMap.classList.add(elementActive);
-  });
-
+  var closeMapButton = modalMap.querySelector('.close-map-btn');
 
   closeMapButton.addEventListener('click', function() {
     modalMap.classList.remove(elementActive);
   });
 
-  closeContactsButton.addEventListener('click', function() {
-    modalContacts.classList.remove(elementActive);
+  openMapButton.addEventListener('click', function() {
+    modalMap.classList.add(elementActive);
   });
 
   window.addEventListener('keydown', function(evt) {
@@ -39,47 +42,36 @@ try {
       }
     }
   });
-
-
-  var changeSlide = function (slideButton, slideName) {
-    slideButton.addEventListener('click', function() {
-      resetSlider();
-      slideButton.classList.add(slideButtonActive);
-      slideName.classList.add(slideActive);
-    })
-  };
-
-
-changeSlide(slideButtonOne, servicesSlideOne);
-changeSlide(slideButtonTwo, servicesSlideTwo);
-changeSlide(slideButtonThree, servicesSlideThree);
-} catch (err) {
-  console.log(err);
 }
 
-var keyCodes = {
-  ESC: 27,
-  ENTER: 13,
-  SPACE: 27
-};
+if (document.querySelector('.services-slides')) {
+  var servicesSlides = document.querySelector('.services-slides');
+  var slideButtonOne = servicesSlides.querySelector('#slide-1');
+  var slideButtonTwo = servicesSlides.querySelector('#slide-2');
+  var slideButtonThree = servicesSlides.querySelector('#slide-3');
+  var servicesSlideOne = servicesSlides.querySelector('.services-slide-1');
+  var servicesSlideTwo = servicesSlides.querySelector('.services-slide-2');
+  var servicesSlideThree = servicesSlides.querySelector('.services-slide-3');
+  var slideButtons = [slideButtonOne, slideButtonTwo, slideButtonThree];
+  var slides = [servicesSlideOne, servicesSlideTwo, servicesSlideThree];
 
-// модальные окна
+  var resetSlider = function () {
+    slideButtons.forEach(function (button) {
+      button.classList.remove(slideButtonActive);
+    })
+    slides.forEach(function (slide) {
+      slide.classList.remove(slideActive);
+    });
+  };
 
-var modalMap = document.querySelector('.modal-map');
-var modalContacts = document.querySelector('.modal-contacts');
-var openMapButton = document.querySelector('.map-btn');
-var openContactsButton = document.querySelector('.contacts-btn');
+  changeSlide(slideButtonOne, servicesSlideOne);
+  changeSlide(slideButtonTwo, servicesSlideTwo);
+  changeSlide(slideButtonThree, servicesSlideThree);
+}
 
-var elementActive = 'element-active';
-
-// открытие/закрытие карты
-
-
-
-
-
-
-// открытие/закрытие формы обратной связи
+closeContactsButton.addEventListener('click', function() {
+  modalContacts.classList.remove(elementActive);
+});
 
 openContactsButton.addEventListener('click', function() {
   modalContacts.classList.add(elementActive);
@@ -94,43 +86,9 @@ window.addEventListener('keydown', function(evt) {
   }
 });
 
-
-// работа слайдеров
-
-var resetSlider = function () {
-  slideButtons.forEach(function (button) {
-    button.classList.remove(slideButtonActive);
-  })
-  slides.forEach(function (slide) {
-    slide.classList.remove(slideActive);
-  });
-};
-
-
-
-
-var modalContacts = document.querySelector('.modal-contacts');
-var pageBody = document.querySelector('body');
-var closeContactsButton = modalContacts.querySelector('.close-contacts-btn');
-
-var elementActive = 'element-active';
-
 pageBody.addEventListener('click', function(evt) {
   var target = evt.target;
   if (target.classList.contains('contacts-btn')) {
     modalContacts.classList.add(elementActive);
   }
-});
-
-window.addEventListener('keydown', function(evt) {
-  if (evt.keyCode === keyCodes.ESC) {
-    evt.preventDefault();
-    if (modalContacts.classList.contains(elementActive)) {
-      modalContacts.classList.remove(elementActive);
-    }
-  }
-});
-
-closeContactsButton.addEventListener('click', function() {
-  modalContacts.classList.remove(elementActive);
 });
